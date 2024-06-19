82d37598ee497d27226a81ce8700785b4533e178
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-19 08:50:34 +0530

    CORS Policy Added

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');  // Importing the CORS middleware
 const config = require('./config');
 const customerRoutes = require('./routes/CustomerRoutes');
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 8080;
 // Middleware
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use(cors());  // Using the CORS middleware
 
 // Routes
 app.use('/api/v1/customer', customerRoutes);
