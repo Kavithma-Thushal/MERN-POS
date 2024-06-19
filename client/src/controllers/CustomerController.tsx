89c0b7a872ef1f462a8093f0e52a2e92429ed467
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-19 15:20:38 +0530

    CustomerController.tsx Added

--- client/src/controllers/CustomerController.tsx
+++ client/src/controllers/CustomerController.tsx
@@ -1,7 +1,101 @@
+import {useState} from "react";
+import axios from "axios";
+
 export function CustomerController() {
-    return (
-        <div>
-            <h1>This is CustomerController</h1>
-        </div>
-    );
+
+    const baseUrl = "http://localhost:8080/api/v1/customer";
+
+    interface Customer {
+        id: string;
+        name: string;
+        address: string;
+        salary: string;
+    }
+
+    const [customerId, setCustomerId] = useState<string>('');
+    const [customerName, setCustomerName] = useState<string>('');
+    const [customerAddress, setCustomerAddress] = useState<string>('');
+    const [customerSalary, setCustomerSalary] = useState<string>('');
+    const [customers, setCustomers] = useState<Customer[]>([]);
+
+    const saveCustomer = async () => {
+        const customerOb = {
+            id: customerId,
+            name: customerName,
+            address: customerAddress,
+            salary: customerSalary
+        };
+
+        try {
+            await axios.post(`${baseUrl}/saveCustomer`, customerOb, {headers: {'Content-Type': 'application/json'}});
+            alert("Customer Saved Successfully...!");
+        } catch (error) {
+            alert("Customer Save Error...!");
+        }
+    };
+
+    const searchCustomer = async () => {
+        try {
+            const response = await axios.get<Customer>(`${baseUrl}/searchCustomer/${customerId}`);
+            const customer = response.data;
+            setCustomerName(customer.name);
+            setCustomerAddress(customer.address);
+            setCustomerSalary(customer.salary);
+            console.log("Customer Searched Successfully...!");
+        } catch (error) {
+            console.log("Customer Search Error...!", error);
+        }
+    };
+
+    const updateCustomer = async () => {
+        const customerOb = {
+            id: customerId,
+            name: customerName,
+            address: customerAddress,
+            salary: customerSalary
+        };
+
+        try {
+            await axios.put(`${baseUrl}/updateCustomer`, customerOb, {headers: {'Content-Type': 'application/json'}});
+            alert("Customer Updated Successfully...!");
+        } catch (error) {
+            alert("Customer Update Error...!");
+        }
+    };
+
+    const deleteCustomer = async () => {
+        try {
+            await axios.delete(`${baseUrl}/deleteCustomer/${customerId}`);
+            alert("Customer Deleted Successfully...!");
+        } catch (error) {
+            alert("Customer Delete Error...!");
+        }
+    };
+
+    const loadAllCustomers = async () => {
+        try {
+            const response = await axios.get<Customer[]>(`${baseUrl}/loadAllCustomers`);
+            setCustomers(response.data);
+        } catch (error) {
+            console.log("Load All Customers Error...!", error);
+        }
+    };
+
+    return {
+        customerId,
+        customerName,
+        customerAddress,
+        customerSalary,
+        customers,
+        setCustomerId,
+        setCustomerName,
+        setCustomerAddress,
+        setCustomerSalary,
+
+        saveCustomer,
+        searchCustomer,
+        updateCustomer,
+        deleteCustomer,
+        loadAllCustomers
+    };
 }
--- client/src/pages/CustomerPage.tsx
+++ client/src/pages/CustomerPage.tsx
@@ -1,86 +1,24 @@
-import {useState} from 'react';
-import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-const baseUrl = "http://localhost:8080/api/v1/customer";
-
-interface Customer {
-    id: string;
-    name: string;
-    address: string;
-    salary: string;
-}
+import {CustomerController} from "../controllers/CustomerController";
 
 export function CustomerPage() {
 
-    const [customerId, setCustomerId] = useState<string>('');
-    const [customerName, setCustomerName] = useState<string>('');
-    const [customerAddress, setCustomerAddress] = useState<string>('');
-    const [customerSalary, setCustomerSalary] = useState<string>('');
-    const [customers, setCustomers] = useState<Customer[]>([]);
-
-    const saveCustomer = async () => {
-        const customerOb = {
-            id: customerId,
-            name: customerName,
-            address: customerAddress,
-            salary: customerSalary
-        };
-
-        try {
-            await axios.post(`${baseUrl}/saveCustomer`, customerOb, {headers: {'Content-Type': 'application/json'}});
-            alert("Customer Saved Successfully...!");
-        } catch (error) {
-            alert("Customer Save Error...!");
-        }
-    };
-
-    const searchCustomer = async () => {
-        try {
-            const response = await axios.get<Customer>(`${baseUrl}/searchCustomer/${customerId}`);
-            const customer = response.data;
-            setCustomerName(customer.name);
-            setCustomerAddress(customer.address);
-            setCustomerSalary(customer.salary);
-            console.log("Customer Searched Successfully...!");
-        } catch (error) {
-            console.log("Customer Search Error...!", error);
-        }
-    };
-
-    const updateCustomer = async () => {
-        const customerOb = {
-            id: customerId,
-            name: customerName,
-            address: customerAddress,
-            salary: customerSalary
-        };
-
-        try {
-            await axios.put(`${baseUrl}/updateCustomer`, customerOb, {headers: {'Content-Type': 'application/json'}});
-            alert("Customer Updated Successfully...!");
-        } catch (error) {
-            alert("Customer Update Error...!");
-        }
-    };
-
-    const deleteCustomer = async () => {
-        try {
-            await axios.delete(`${baseUrl}/deleteCustomer/${customerId}`);
-            alert("Customer Deleted Successfully...!");
-        } catch (error) {
-            alert("Customer Delete Error...!");
-        }
-    };
-
-    const loadAllCustomers = async () => {
-        try {
-            const response = await axios.get<Customer[]>(`${baseUrl}/loadAllCustomers`);
-            setCustomers(response.data);
-        } catch (error) {
-            console.log("Load All Customers Error...!", error);
-        }
-    };
+    const {
+        customerId,
+        customerName,
+        customerAddress,
+        customerSalary,
+        customers,
+        setCustomerId,
+        setCustomerName,
+        setCustomerAddress,
+        setCustomerSalary,
+        saveCustomer,
+        searchCustomer,
+        updateCustomer,
+        deleteCustomer,
+        loadAllCustomers
+    } = CustomerController();
 
     return (
         <main className="container my-5">
