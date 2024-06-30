9a65f24befe85f0506c3da5047202752003e2e8d
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-30 14:37:09 +0530

    Remote DB Added

--- server/src/config.js
+++ server/src/config.js
@@ -1,3 +1,8 @@
 module.exports = {
-    mongoURI: "mongodb://localhost:27017/demo"
+
+    // local
+    // mongoURI: "mongodb://localhost:27017/demo"
+
+    // remote
+    // mongoURI: "mongodb+srv://root:1234@cluster0.bc8jji6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 };
