67143706041a4d04b3371590818c54d3db878812
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-07-10 23:01:09 +0530

    DB Config Refactored

--- server/src/config.js
+++ server/src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     // local
-    // mongoURI: "mongodb://localhost:27017/demo"
+    mongoURI: "mongodb://localhost:27017/demo"
 
     // remote
     // mongoURI: "mongodb+srv://root:1234@cluster0.bc8jji6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
